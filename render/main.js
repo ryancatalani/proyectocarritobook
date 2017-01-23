05f797885350c8d7b13b1b15c0852f647f416962
Ryan Catalani <catalani.ryan@gmail.com>
2017-01-22 21:31:21 -0500

    Add language switching.

--- render/main.js
+++ render/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
+	var globalLang;
+	if (Cookies.get('lang') !== undefined) {
+		globalLang = Cookies.get('lang');
+	} else {
+		globalLang = 'or';
+	}
+	setLang(globalLang);
 	
+	$('li.lang').click(function() {
+		if ($(this).hasClass('active')) return;
+		
+		var newLang = $(this).data('lang');
+		setLang(newLang);
+	});
 
+	function setLang(newLang) {
+		// var currentLang = globalLang;
+		var windowPosition = $(window).scrollTop();
+
+		langContents('or').hide();
+		langContents('en').hide();
+		langContents('es').hide();
+
+		langContents(newLang).show();
+		$(window).scrollTop(windowPosition);
+
+		$('#lang_switcher').find('.active').removeClass('active');
+		$('.lang[data-lang="' + newLang + '"]').addClass('active');
+		globalLang = newLang;
+		Cookies.set('lang', globalLang, { expires: 365 });
+	}
+
+	function langContents(lang) {
+		return $('.content_' + lang);
+	}
 })
